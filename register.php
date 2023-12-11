511b2ae861e7de07f11d8a9b4cc3ffb606a04595
brian blake <104259527+babyslikk@users.noreply.github.com>
2023-12-11 01:59:36 -0600

    Added styling & username, password, email to the register page

---
+++ register.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Jazzyflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+    </head>
+    <body>
+
+        <div class="signInContainer">
+
+            <div class="column">
+                <form method="POST"> 
+                    <input type="text" name="firstName" placeholder="First name" required>
+
+                    <input type="text" name="lastName" placeholder="Last name" required>
+
+                    <input type="text" name="userName" placeholder="Username" required>
+
+                    <input type="email" name="email" placeholder="Email" required>
+
+                    <input type="email" name="email2" placeholder="Confirm email" required>
+
+                    <input type="password" name="password" placeholder="Password" required>
+
+                    <input type="password" name="password" placeholder="Confirm password" required>
+
+                    <input type="submit" name="submitPassword" value="SUBMIT" required>
+
+                </form>
+
+            </div>
+        
+        </div>    
+       
+    </body>
+</html>
